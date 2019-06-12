9b92ae2cd91e0c94f7370c1071e201c2487561ef
chrisnojima <cnojima@keyba.se>
2019-06-12 17:13:12 -0400

    update lifecycles

--- src/Gateway.js
+++ src/Gateway.js
@@ -1,44 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import GatewayRegistry from './GatewayRegistry';
+import * as React from 'react';
+import {GatewayContext} from './GatewayProvider';
 
-export default class Gateway extends React.Component {
-  static contextTypes = {
-    gatewayRegistry: PropTypes.instanceOf(GatewayRegistry).isRequired
-  };
-
-  static propTypes = {
-    into: PropTypes.string.isRequired,
-    children: PropTypes.node
-  };
-
-  constructor(props, context) {
-    super(props, context);
-    this.gatewayRegistry = context.gatewayRegistry;
-  }
-
-  componentWillMount() {
-    this.id = this.gatewayRegistry.register(
+class Gateway extends React.Component {
+  componentDidMount() {
+    this.id = this.props.gatewayRegistry.register(
       this.props.into,
       this.props.children
     );
     this.renderIntoGatewayNode(this.props);
   }
 
-  componentWillReceiveProps(props) {
-    this.gatewayRegistry.clearChild(this.props.into, this.id);
+  componentDidUpdate(props) {
+    this.props.gatewayRegistry.clearChild(this.props.into, this.id);
     this.renderIntoGatewayNode(props);
   }
 
   componentWillUnmount() {
-    this.gatewayRegistry.unregister(this.props.into, this.id);
+    this.props.gatewayRegistry.unregister(this.props.into, this.id);
   }
 
   renderIntoGatewayNode(props) {
-    this.gatewayRegistry.addChild(this.props.into, this.id, props.children);
+    this.props.gatewayRegistry.addChild(this.props.into, this.id, props.children);
   }
 
-  render() {
-    return null;
-  }
+  render() { return null; }
 }
+
+export default (props) => (
+    <GatewayContext.Consumer>
+        {({gatewayRegistry}) => <Gateway gatewayRegistry={gatewayRegistry} {...props} />}
+    </GatewayContext.Consumer>
+);
